2fccdcfa74c14b2258dc51dbfbb2bc3347031121
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-06-21 10:02:47 -0300

    feat: instantiating ejs templates

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Tracert - Software House')
-})
+    res.render('index');
+});
 
 app.listen(port, () => {
-    console.log(`App runnig on port ${port}`)
-})
+    console.log(`App runnig on port ${port}`);
+});
